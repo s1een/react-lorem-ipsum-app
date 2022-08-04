11d7f26e700d374842e2fc58955a2bf177666504
r3ason_why <windeks2017@gmail.com>
2022-08-04 17:39:03 +0300

    React Lorem App Deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchText();
+  }
+  async function fetchText() {
+    let tmp = count;
+    if (tmp <= 0) {
+      tmp = 1;
+    }
+    const response = await fetch(
+      `https://hipsum.co/api/?type=hipster-centric&paras=${tmp}`
+    );
+    const data = await response.json();
+    setText(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
   );
 }
 
